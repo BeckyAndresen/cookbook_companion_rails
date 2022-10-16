31acc2f57d938eda4d60eb992223304acb78f271
Becky Andresen <rdandresen@gmail.com>
2022-10-16 18:56:46 -0500

    added db seeds and fixtures

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+bread = Ingredient.create(name: 'bread')
+peanut_butter = Ingredient.create(name: 'peanut butter')
+jelly = Ingredient.create(name: 'jelly')
+hummus = Ingredient.create(name: 'hummus')
+tomato = Ingredient.create(name: 'tomato')
+cucumber = Ingredient.create(name: 'cucumber')
+lettuce = Ingredient.create(name: 'lettuce')
+
+
+recipe1 = Recipe.create(name: 'Peanut Butter and Jelly Sandwich', page_number: 1)
+recipe1.ingredients << [bread, peanut_butter, jelly]
+
+recipe2 = Recipe.create(name: 'Hummus and Veggies Sandwich', page_number: 2)
+recipe2.ingredients << [bread, hummus, tomato, lettuce, cucumber]
+
+recipe3 = Recipe.create(name: 'Salad', page_number: 3)
+recipe3.ingredients << [tomato, lettuce, cucumber]
+
+recipe4 = Recipe.create(name: 'Peanut Butter Sandwich', page_number: 4)
+recipe4.ingredients << [bread, peanut_butter]
